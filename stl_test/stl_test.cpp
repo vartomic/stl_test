866571aa9ceeb20424d43990a79c290ea72eaa43
vartomic <varrava16@mail.ru>
2024-03-30 17:04:41 +0300

    Changed func all_of, added user function and number as arguments, removed everything else

--- stl_test/stl_test.cpp
+++ stl_test/stl_test.cpp
@@ -3,32 +3,32 @@
 
 #include <iostream>
 
-bool all_of(int* arr, int num, int size) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] != num) {
-            return false;
-        }
-        else if (arr[i] == num && i == size-1) {
-            return true;
-        }
+bool isPositive(int num) {
+    if (num < 0) {
+        return false;
+    }
+}
+
+bool isNegative(int num) {
+    if (num > 0) {
+        return false;
+    }
+}
+
+bool isEven(int num) {
+    if (num % 2 != 0) {
+        return false;
+    }
+}
+
+bool funcTest(bool userFunc(int par), int val) {
+    if (userFunc(val) != true) {
+        return false;
     }
 }
 
 int main() {
-    int arr[3] = { 4, 4, 4};
-    int num = 4;
-    int size = sizeof(arr) / sizeof(arr[0]);
-    all_of(arr, num, size);
-
+    bool res1 = funcTest(isPositive, 123);
+    bool res2 = funcTest(isNegative, -1);
+    bool res3 = funcTest(isEven, 2);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
